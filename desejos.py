c30028460db5082ae1a3bf1fd8d8653b666b924d
Carlos Nassif <costa.carlos12@gmail.com>
2019-10-26 12:09:21 -0300

    Update .1
    Alteração do nome das variáveis e adição de comentários.

--- desejos.py
+++ desejos.py
@@ -1,43 +1,44 @@
+# tryAppend() testa se tem espaco em algum lado (2 opções) da criança
 def tryAppend(a, b, grafo):
-    if b not in grafo[a]:
-        if len(grafo[a]) == 2:
-            return False
-        else:
-            grafo[a].append(b)
-            grafo[b].append(a)
-    return True
+	if b not in grafo[a]:
+		if len(grafo[a]) == 2:
+			return False
+		else:
+			grafo[a].append(b)
+			grafo[b].append(a)
+	return True
 
-def RIPdream():
-    k, w = map(int, input().split())
-    while k >=3 and k <= 10**9 and w>=0 and w<=10**5:
-        if w<=2:
-            print("Y")
-        else:
-            grafo = {x:[] for x in range(1,k+1)}
-            for i in range(w):
-                a, b = map(int, input().split())
-                sucesso = tryAppend(a,b,grafo)
-                if not sucesso:
-                    break
-            #print(grafo)
-            osDeFora = []
-            temEspaco = []
-            if sucesso:
-                for i in range(1,len(grafo)+1):
-                    if len(grafo[i])==0:
-                        osDeFora.append(i)
-                    if len(grafo[i])==1:
-                        temEspaco.append(i)
-                if len(temEspaco) < 1 and len(osDeFora) > 0:
-                    sucesso = False
-            
-            if sucesso:
-                print("Y")
-            else:
-                print("N")
-                
-                
+# main()
+def main():
+	k, w = map(int, input().split())
+	while k >=3 and k <= 10**9 and w>=0 and w<=10**5:
+		if w<=2:
+			print("Y")
+		else:
 
-        k, w = map(int, input().split())
+			grafo = {x:[] for x in range(1,k+1)}
+			for i in range(w):
+				a, b = map(int, input().split())
+				sucesso = tryAppend(a,b,grafo) # guarda se {deu pra inseir ou ja inseriu} (True) ou {não} (False)
+				if not sucesso:
+					break
 
-RIPdream()
+			if sucesso:
+				ListaDeFora = []
+				ListaTemEspaco = []
+				for i in range(1,len(grafo)+1):
+					if len(grafo[i])==0:
+						ListaDeFora.append(i)
+					elif len(grafo[i])==1:
+						ListaTemEspaco.append(i)
+				if len(ListaTemEspaco) < 1 and len(ListaDeFora) > 0:
+					sucesso = False
+
+			if sucesso:
+				print("Y")
+			else:
+				print("N")
+
+		k, w = map(int, input().split())
+
+main()
